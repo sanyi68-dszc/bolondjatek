78498f2546495b9b605e1a2506cb18e0490716d3
Sajat Nev <email@pelda.hu>
2026-03-18 13:24:02 +0100

    Tábla színek módosítása

--- alap.js
+++ alap.js
@@ -8,7 +8,7 @@ function createTable(){
         oszlopTomb=[]
         for (let j = 0; j < oszlop; j++) {
             oszlopTomb.push(j)
-            sz+=`<button style="width:50px; height:50px; background-color:lightgray;" onclick="colorButton(this.id)" id="${i}-${j}">${i+1},${j+1}</button>`
+            sz+=`<button style="width:50px; height:50px; background-color:lightblue;" onclick="colorButton(this.id)" id="${i}-${j}">${i+1},${j+1}</button>`
         }
         sz+=`<br>`
         sorTomb.push(i)
@@ -20,22 +20,22 @@ function colorButton(id){
     idTomb=id.split('-')
     i=parseInt(idTomb[0])
     j=parseInt(idTomb[1])
-    if(document.getElementById(id).style.backgroundColor=="lightgray") document.getElementById(id).style.backgroundColor="red"
-    else document.getElementById(id).style.backgroundColor="lightgray"
+    if(document.getElementById(id).style.backgroundColor=="lightblue") document.getElementById(id).style.backgroundColor="purple"
+    else document.getElementById(id).style.backgroundColor="lightblue"
     if(i-1>=0){
-        if(document.getElementById(`${i-1}-${j}`).style.backgroundColor=="lightgray") document.getElementById(`${i-1}-${j}`).style.backgroundColor="red"
-        else document.getElementById(`${i-1}-${j}`).style.backgroundColor="lightgray"
+        if(document.getElementById(`${i-1}-${j}`).style.backgroundColor=="lightblue") document.getElementById(`${i-1}-${j}`).style.backgroundColor="purple"
+        else document.getElementById(`${i-1}-${j}`).style.backgroundColor="lightblue"
     }
     if(j-1>=0){
-        if(document.getElementById(`${i}-${j-1}`).style.backgroundColor=="lightgray") document.getElementById(`${i}-${j-1}`).style.backgroundColor="red"
-        else document.getElementById(`${i}-${j-1}`).style.backgroundColor="lightgray"
+        if(document.getElementById(`${i}-${j-1}`).style.backgroundColor=="lightblue") document.getElementById(`${i}-${j-1}`).style.backgroundColor="purple"
+        else document.getElementById(`${i}-${j-1}`).style.backgroundColor="lightblue"
     }
     if(j+1<oszlop){
-        if(document.getElementById(`${i}-${j+1}`).style.backgroundColor=="lightgray") document.getElementById(`${i}-${j+1}`).style.backgroundColor="red"
-        else document.getElementById(`${i}-${j+1}`).style.backgroundColor="lightgray"
+        if(document.getElementById(`${i}-${j+1}`).style.backgroundColor=="lightblue") document.getElementById(`${i}-${j+1}`).style.backgroundColor="purple"
+        else document.getElementById(`${i}-${j+1}`).style.backgroundColor="lightblue"
     }
     if(i+1<sor){
-        if(document.getElementById(`${i+1}-${j}`).style.backgroundColor=="lightgray") document.getElementById(`${i+1}-${j}`).style.backgroundColor="red"
-        else document.getElementById(`${i+1}-${j}`).style.backgroundColor="lightgray"
+        if(document.getElementById(`${i+1}-${j}`).style.backgroundColor=="lightblue") document.getElementById(`${i+1}-${j}`).style.backgroundColor="purple"
+        else document.getElementById(`${i+1}-${j}`).style.backgroundColor="lightblue"
     }
 }
